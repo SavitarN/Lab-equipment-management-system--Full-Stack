640080ca1033049651210514aa035b6f40ee3ee9
Nischal Joshi <87236089+SavitarN@users.noreply.github.com>
2023-10-06 20:13:01 +0545

    Add files via upload

---
+++ admin_area/view_products.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    .tbl {
+        margin: 0px 317px;
+    }
+
+    .th {
+        padding: 10px;
+    }
+    </style>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <table class="tbl" border="1">
+        <thead>
+            <tr>
+                <th class="th">Product ID</th>
+                <th class="th">Product Name</th>
+                <th class="th">Product Image</th>
+                <th class="th">Product Price</th>
+                <th class="th">Status</th>
+                <th class="th">Edit</th>
+                <th class="th">Delete</th>
+
+
+            </tr>
+        </thead>
+
+
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
